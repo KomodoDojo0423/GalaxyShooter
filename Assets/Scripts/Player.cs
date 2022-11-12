1c3f6b99ddf958b20add53c41874875446540cef
KomodoDojo <travis.gebo91@gmail.com>
2022-11-11 23:21:58 -0500

    Adding player movement script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //take the current position = new position (0, 0, 0)
+       transform.position = new Vector3(0, 0, 0); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //Player movement inputs
+      float horizontalInput = Input.GetAxis("Horizontal"); 
+      float verticalInput = Input.GetAxis("Vertical");
+
+      transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+      transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+      //Player bounds for Y axis
+      if(transform.position.y >= 0)
+      {
+        transform.position = new Vector3(transform.position.x, 0, 0);
+      }
+      else if(transform.position.y <= -3.8f)
+      {
+        transform.position = new Vector3(transform.position.x, -3.8f, 0);
+      }
+
+      //Player bounds/wrap around for x axis
+      if(transform.position.x >= 11.5f)
+      {
+        transform.position = new Vector3(-11.5f, transform.position.y, 0);
+      }
+      else if(transform.position.x <= -11.5f)
+      {
+        transform.position = new Vector3(11.5f, transform.position.y, 0);
+      }
+  
+    }
+}
