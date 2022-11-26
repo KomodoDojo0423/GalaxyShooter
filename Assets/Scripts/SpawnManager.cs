8b6b0b3f8c645165a33af7818c30481a1b5f4059
KomodoDojo <travis.gebo91@gmail.com>
2022-11-25 21:39:21 -0500

    Scripted Spawn Manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemy;
+    [SerializeField]
+    private GameObject _enemyContainer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine());
     }
 
     // Update is called once per frame
@@ -15,4 +19,16 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5.0f);
+            GameObject newEnemy = Instantiate(_enemy, new Vector3 (Random.Range(-8.5f, 8.5f), 7.5f, 0), Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+        }
+        
+    }
+
 }
